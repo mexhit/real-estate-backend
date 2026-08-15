b28e569f9dcd25884e53832d9ec474597e685b1b
mexhit <mexhii.asllani@gmail.com>
2026-08-15 11:53:33 +0200

    Prettier format ecosystem.config.js file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   apps: [
     {
-	    name: "real-estate-backend",
-      script: "dist/main.js",
-      instances: "max",
-      exec_mode: "cluster",
+      name: 'real-estate-backend',
+      script: 'dist/main.js',
+      instances: 'max',
+      exec_mode: 'cluster',
       watch: false,
       env_production: {
-        NODE_ENV: "production"
-      }
-    }
-  ]
+        NODE_ENV: 'production',
+      },
+    },
+  ],
 };
